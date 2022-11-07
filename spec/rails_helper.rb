ea1c7b4efdd9dc017fca42162b80e70ab1b10d95
Alex Withington-Smith <alex@awithsmith.com>
2022-11-07 16:37:06 +0000

    Add RuboCop configuration

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,13 +1,15 @@
+# frozen_string_literal: true
+
 # This file is copied to spec/ when you run 'rails generate rspec:install'
-require 'spec_helper'
-ENV['RAILS_ENV'] ||= 'test'
-require_relative '../config/environment'
+require "spec_helper"
+ENV["RAILS_ENV"] ||= "test"
+require_relative "../config/environment"
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
-require 'rspec/rails'
-require 'support/factory_bot'
-require 'support/shoulda_matchers'
-require 'support/database_cleaner'
+require "rspec/rails"
+require "support/factory_bot"
+require "support/shoulda_matchers"
+require "support/database_cleaner"
 
 # Add additional requires below this line. Rails is not loaded until this point!
 
