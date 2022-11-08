050c3795aec01890121c09a05f3c6c585cc63a31
Alex Withington-Smith <alex@awithsmith.com>
2022-11-08 09:15:11 +0000

    Got rid of a lot of the test baggage

--- spec/features/user_creates_submission_spec.rb
+++ spec/features/user_creates_submission_spec.rb
@@ -3,12 +3,11 @@
 require "rails_helper"
 require "faker"
 
-RSpec.describe "submissions/new", type: :view do
+RSpec.feature "Creating new Submission", type: :feature do
   let!(:item_name) { Faker::Educator.course_name }
 
   it "populates hidden item field from query params" do
     visit new_submission_path prefill_item: item_name
-
     expect(page).to have_field "submission_item", type: :hidden, with: item_name
   end
 end
