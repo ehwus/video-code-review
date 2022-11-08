4a782e389411a19756b0b73057881f28acf446a3
Alex Withington-Smith <alex@awithsmith.com>
2022-11-08 09:25:48 +0000

    Added full test for creating a submission

--- spec/features/user_creates_submission_spec.rb
+++ spec/features/user_creates_submission_spec.rb
@@ -15,4 +15,14 @@ RSpec.feature "Creating new Submission", type: :feature do
     visit new_submission_path
     expect(page).to have_field "submission_item", type: :hidden, with: "unspecified"
   end
+
+  it "allows the user to successfully create a Submission" do
+    visit new_submission_path prefill_item: item_name
+    fill_in 'submission_full_name', with: "Partario"
+    fill_in 'submission_reflections', with: "It was good thanks"
+    attach_file('submission_video', 'test/fixtures/files/example.mp4')
+    click_button 'Create'
+
+    expect(Submission.all.length).to eq(1)
+  end
 end
