5a12abf52b5372b57a038bec497db38c84d05f99
Alex Withington-Smith <alex@awithsmith.com>
2022-11-09 14:32:01 +0000

    Review nudge always prioritises submissions with no reviews

--- app/controllers/review_prompter_controller.rb
+++ app/controllers/review_prompter_controller.rb
@@ -9,7 +9,9 @@ class ReviewPrompterController < ApplicationController
     def get_submission_to_review(owned_submission_id)
       item_name = Submission.find(owned_submission_id).item
       relevant_submissions = Submission.where(item: item_name).where.not(id: owned_submission_id)
+      submissions_without_reviews = relevant_submissions.filter { |submission| submission.reviews.empty? }
+      return submissions_without_reviews.sample unless submissions_without_reviews.empty?
 
-      return relevant_submissions.sample unless relevant_submissions.empty?
+      relevant_submissions.sample unless relevant_submissions.empty?
     end
 end
--- spec/features/user_creates_submission_spec.rb
+++ spec/features/user_creates_submission_spec.rb
@@ -29,30 +29,42 @@ RSpec.feature "Creating new Submission", type: :feature do
     expect(Submission.all.length).to eq(1)
   end
 
-  it "redirects user to a confirmation page with no prompt if there are no other submissions to be reviewed" do
-    create_new_submission
-    expect(page).to have_text("Your video has been submitted!")
-    expect(page).to have_text("We will send you a notification via email and Slack once it has been reviewed.")
-    expect(page).not_to have_text("Do you have time to give a review? Reviewing other's work is a great way to refine your own mentoring and feedback capabilities.")
-    expect(page).not_to have_link("Review a colleague's work")
-  end
+  describe "review prompt" do
+    it "redirects user to a confirmation page with no prompt if there are no other submissions to be reviewed" do
+      create_new_submission
+      expect(page).to have_text("Your video has been submitted!")
+      expect(page).to have_text("We will send you a notification via email and Slack once it has been reviewed.")
+      expect(page).not_to have_text("Do you have time to give a review? Reviewing other's work is a great way to refine your own mentoring and feedback capabilities.")
+      expect(page).not_to have_link("Review a colleague's work")
+    end
 
-  it "has a prompt to review a random submission if another exists without reviews" do
-    submission = create(:submission, item: item_name)
+    it "has a prompt to review a random submission if another exists without reviews" do
+      submission = create(:submission, item: item_name)
 
-    create_new_submission(item_name)
-    expect(page).to have_text("Your video has been submitted!")
-    expect(page).to have_text("We will send you a notification via email and Slack once it has been reviewed.")
-    expect(page).to have_text("Do you have time to give a review? Reviewing others' work is a great way to refine your own mentoring and feedback capabilities.")
-    expect(page).to have_link("Review a colleague's work", href: submission_path(submission.id))
-  end
+      create_new_submission(item_name)
+      expect(page).to have_text("Your video has been submitted!")
+      expect(page).to have_text("We will send you a notification via email and Slack once it has been reviewed.")
+      expect(page).to have_text("Do you have time to give a review? Reviewing others' work is a great way to refine your own mentoring and feedback capabilities.")
+      expect(page).to have_link("Review a colleague's work", href: submission_path(submission.id))
+    end
 
-  it "only asks a student to review a submission of the same curriculum item" do
-    create(:submission, item: "item_name")
-    create_new_submission("completely_different_item_name")
-    expect(page).to have_text("Your video has been submitted!")
-    expect(page).to have_text("We will send you a notification via email and Slack once it has been reviewed.")
-    expect(page).not_to have_text("Do you have time to give a review? Reviewing other's work is a great way to refine your own mentoring and feedback capabilities.")
-    expect(page).not_to have_link("Review a colleague's work")
+    it "only asks a student to review a submission of the same curriculum item" do
+      create(:submission, item: "item_name")
+      create_new_submission("completely_different_item_name")
+      expect(page).to have_text("Your video has been submitted!")
+      expect(page).to have_text("We will send you a notification via email and Slack once it has been reviewed.")
+      expect(page).not_to have_text("Do you have time to give a review? Reviewing other's work is a great way to refine your own mentoring and feedback capabilities.")
+      expect(page).not_to have_link("Review a colleague's work")
+    end
+
+    it "always prioritises submissions with no reviews" do
+      create(:submission, item: item_name, reviews: [create(:review), create(:review), create(:review)])
+      submission = create(:submission, item: item_name)
+      create_new_submission(item_name)
+      10.times do
+        visit current_path
+        expect(page).to have_link("Review a colleague's work", href: submission_path(submission.id))
+      end
+    end
   end
 end
